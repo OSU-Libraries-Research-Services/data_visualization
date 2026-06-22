7c0e5faf9c27676f00371e7f4641384accd07e39
Sarah Murphy <murphy.465@osu.edu>
2026-06-22 16:32:26 -0400

    updated content

---
+++ python_mastering_basics_workshop_exercises.py
@@ -0,0 +1,130 @@
+# ********** VARIABLES **********
+
+
+# ========== EXERCISE 1: FIND DATA TYPE ==========
+institution_name = "The Ohio State University"
+year_founded = 1870
+university_facts = {"institution_name" : "The Ohio State University", "year_founded" : 1870, "school_colors" : "scarlet & grey", "mascot" : "Brutus Buckeye" , 
+                    "students" : 60046 , "endowment": 7.9 }
+fact_list = ["The Ohio State University" , 1870, "scarlet & grey" , "Brutus Buckeye", 60046, 7.9 ]
+
+"""Ask CoPilot how to find the data type for the variable institution_name."""
+
+# ********** STRINGS **********
+
+
+# ========== EXERCISE 2: SLICE CARMEN OHIO ==========
+"""Ask CoPilot how to open and read a .txt file. Then open and read the file carmen_ohio.txt and ...
+1. Find the first 250 characters in Carmen Ohio
+2. Find the last 250 characters in Carmen Ohio
+3. Find the second sentence in Carmen Ohio"""
+
+
+# ********** READING AND WRITING FILES **********
+
+
+# ========== EXERCISE 3: STRINGS ==========
+"""
+1. Isolate a sentence in carmen_ohio
+2. Print the isolated sentence
+3. Create a new sentence and try replacing a word
+4. Print the new sentence in uppercase text"""
+
+
+# ********** COMMON STRING METHODS **********
+
+
+# ********** CONDITIONALS AND COMPARISONS **********
+
+
+# ========== EXERCISE 4: CONDITIONALS AND COMPARISONS ==========
+wilbur_age = 1
+charlotte_age = 4
+farmer_age = 42
+
+"""Is Wilbur older than Charlotte? Verify Wilbur's age with a conditional statement."""
+
+
+# ********** LOGICAL OPERATORS **********
+
+
+# ********** LISTS AND LOOPS **********
+
+
+# ========== EXERCISE 5: EXTRACT LIST ITEMS ==========
+characters = ['Han Solo', 'Luke Skywalker', 'C3P0', 'R2D2']
+
+"""Is there another method to extract the last 3 items from the list characters?"""
+
+
+# ========== EXERCISE 6: STEP THROUGH A LIST ==========
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+"""Extract every second item from the list numbers."""
+
+
+# ********** COMMON LIST METHODS **********
+
+
+# ********** USING LOOPS WITH LISTS **********
+
+
+# ========== EXERCISE 7: LOOPS ==========
+presidents=["Edward Francis Baxter Orton Sr","Walter Quincy Scott","William Henry Scott",
+            "James Hulme Canfield","William Oxley Thompson","George Washington Rightmire",
+            "William McPherson","Howard Landis Bevis","Novice Gail Fawcett","Harold Leroy Enarson",
+            "Edward Harrington Jennings","E. Gordon Gee","John Richard Sisson",
+            "William English Kirwan","Edward Harrington Jennings","Karen Ann Holbrook",
+            "Joseph A. Alutto","E. Gordon Gee","Joseph A. Alutto","Michael V. Drake",
+            "Kristina M. Johnson","Peter J. Mohler",'Walter "Ted" Carter, Jr.',"Ravi Bellamkonda"]
+
+"""Print the name of each president of The Ohio State University"""
+
+
+# ********** INDEX OR ENUMERATE LIST ITEMS **********
+
+
+
+# ========== EXERCISE 8: COUNTER VARIABLES ==========
+"""Ask CoPilot how to manually track the position of items using a counter variable as you loop through a list.
+This can be especially useful when you need more control over how positions are counted or displayed."""
+
+
+# ********** BUILDING LISTS WITH LOOPS **********
+united_states=["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut",
+               "Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa",
+               "Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan",
+               "Minnesota","Mississippi","Missouri","Montana","Nebraska", "Nevada","New Hampshire",
+               "New Jersey","New Mexico","New York","North Carolina","North Dakota", "Ohio",
+               "Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota",
+               "Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia",
+               "Wisconsin","Wyoming"]
+
+
+# ********** LIST COMPREHENSIONS **********
+
+
+# ********** DICTIONARIES **********
+university_facts = {"institution_name" : "The Ohio State University",
+	"year_founded" : 1870,
+	"school_colors" : "scarlet & gray",
+	"mascot" : "Brutus Buckeye",
+	"students" : 60046,
+	"endowment" : 7.9 }
+print(university_facts)
+
+
+# ********** CREATE A DICTIONARY **********
+
+
+# ********** LOOPING THROUGH A DICTIONARY **********
+
+
+# ********** FUNCTIONS IN PYTHON **********
+
+
+# ========== EXERCISE 9: FUNCTIONS ==========
+"""Make a function that transforms a book title to lower case, then call your function"""
+
+
+# ********** EXPORT DATA TO CSV FILE **********
